1f52001b6305d33b8d9a6e0f1675410cb1446e9b
Aman <aman.swar001@gmail.com>
2025-07-01 02:06:26 +0530

    add multi CPU simulation support

--- util.py
+++ util.py
@@ -3,6 +3,8 @@ from flax.struct import dataclass
 from flax.training import train_state
 from typing import Any , Callable , Dict , Tuple
 
+import os
+
 Metrics = Dict[str , Tuple[jax.Array , ...]]
 
 class TrainState(train_state.TrainState):
@@ -13,4 +15,16 @@ class TrainState(train_state.TrainState):
 class Batch:
     inputs : jax.Array
     labels : jax.Array
+
+
+def sim_multiCPU_dev(
+        device_count : int = 8
+):    
+    flags = os.environ.get("XLA_FLAGS" , "")
+    flags += f" --xla_force_host_platform_device_count={device_count}"
+    
+    os.environ["XLA_FLAGS"] = flags
+    
+    #disable CUDA 
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
