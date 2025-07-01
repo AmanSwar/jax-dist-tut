0ad8bc1e35ec11db727bd9ca84ddbaf055d4d803
Aman <aman.swar001@gmail.com>
2025-07-01 16:20:26 +0530

    add configs

--- data_paral.py
+++ data_paral.py
@@ -35,7 +35,41 @@ def fold_rng_over_axis(
 
 
 
+#configs
+DATA_CONFIG = ConfigDict(
+    dict(
+        batch_size=128,
+        num_classes=10,
+        input_size=784,
+    )
+)
 
+MODEL_CONFIG = ConfigDict(
+    dict(
+        hidden_size=512,
+        dropout_rate=0.1,
+        dtype=jnp.bfloat16,
+        num_classes=DATA_CONFIG.num_classes,
+        data_axis_name="data",
+    )
+)
+
+OPTIMIZER_CONFIG = ConfigDict(
+    dict(
+        learning_rate=1e-3,
+        num_minibatches=4,
+    )
+)
+
+CONFIG = ConfigDict(
+    dict(
+        model= MODEL_CONFIG,
+        optimizer = OPTIMIZER_CONFIG,
+        data = DATA_CONFIG,
+        data_axis_name=MODEL_CONFIG.data_axis_name,
+        seed=69,
+    )
+)
 
 
 class Classifier(nn.Module):
