2cac77978d5aaabcce610decf03007d3780554eb
Aman <aman.swar001@gmail.com>
2025-07-02 17:08:18 +0530

    black format

--- util.py
+++ util.py
@@ -2,69 +2,68 @@ import jax
 import jax.numpy as jnp
 from flax.struct import dataclass
 from flax.training import train_state
-from typing import Any , Callable , Dict , Tuple
+from typing import Any, Callable, Dict, Tuple
 import numpy as np
 
 import textwrap
 from termcolor import colored
 
+
 def print_exception(e):
-  name = colored(f'{type(e).__name__}', 'red', force_color=True)
-  print(textwrap.fill(f'{name}: {str(e)}'))
+    name = colored(f"{type(e).__name__}", "red", force_color=True)
+    print(textwrap.fill(f"{name}: {str(e)}"))
 
 
 Pytree = Any
-Metrics = Dict[str , Tuple[jax.Array , ...]]
+Metrics = Dict[str, Tuple[jax.Array, ...]]
+
 
 class TrainState(train_state.TrainState):
-    rng : jax.Array
+    rng: jax.Array
 
 
 @dataclass
 class Batch:
-    inputs : jax.Array
-    labels : jax.Array
+    inputs: jax.Array
+    labels: jax.Array
 
 
-def sim_multiCPU_dev(
-        device_count : int = 8
-):    
-    flags = os.environ.get("XLA_FLAGS" , "")
+def sim_multiCPU_dev(device_count: int = 8):
+    flags = os.environ.get("XLA_FLAGS", "")
     flags += f" --xla_force_host_platform_device_count={device_count}"
-    
+
     os.environ["XLA_FLAGS"] = flags
-    
-    #disable CUDA 
+
+    # disable CUDA
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 
 def accum_grads_loop(
-        batch : Batch,
-        state : TrainState,
-        key,
-        n_minbatch : int,
-        loss_fn : Callable,
-
-) -> Tuple[Pytree , Metrics]:
+    batch: Batch,
+    state: TrainState,
+    key,
+    n_minbatch: int,
+    loss_fn: Callable,
+) -> Tuple[Pytree, Metrics]:
 
     bs = batch.inputs.shape[0]
     min_batchSize = bs // n_minbatch
-    keys = jax.random.split(key , n_minbatch)
+    keys = jax.random.split(key, n_minbatch)
 
-    grad_fn = jax.value_and_grad(loss_fn , has_aux=True)
+    grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
 
     grads = None
-    metrics : Metrics = None
+    metrics: Metrics = None
 
     for batch_idx in range(n_minbatch):
 
         start_idx = batch_idx * min_batchSize
         end_idx = start_idx + min_batchSize
-        #slicing Batch class 
-        miniBatch = jax.tree_util.tree_map(lambda x : x[start_idx: end_idx] , batch)
+        # slicing Batch class
+        miniBatch = jax.tree_util.tree_map(lambda x: x[start_idx:end_idx], batch)
 
-        (_ , step_metric) , step_grad = grad_fn(
-            state.params , state.apply_fn , miniBatch , keys[batch_idx]
+        (_, step_metric), step_grad = grad_fn(
+            state.params, state.apply_fn, miniBatch, keys[batch_idx]
         )
 
         if grads is None:
@@ -72,94 +71,70 @@ def accum_grads_loop(
             metrics = step_metric
 
         else:
-            grads = jax.tree_util.tree_map(jnp.add , grads , step_grad)
-            metrics = jax.tree_util.tree_map(jnp.add , metrics , step_metric)
-            
-    grads = jax.tree_util.tree_map(lambda g : g / n_minbatch , grads)
-    return grads , metrics
-    
+            grads = jax.tree_util.tree_map(jnp.add, grads, step_grad)
+            metrics = jax.tree_util.tree_map(jnp.add, metrics, step_metric)
+
+    grads = jax.tree_util.tree_map(lambda g: g / n_minbatch, grads)
+    return grads, metrics
+
 
 def accum_grads_scan(
-        batch : Batch,
-        state : TrainState,
-        key,
-        n_minbatch : int,
-        loss_fn : Callable,
+    batch: Batch,
+    state: TrainState,
+    key,
+    n_minbatch: int,
+    loss_fn: Callable,
+) -> Tuple[Pytree, Metrics]:
 
-) -> Tuple[Pytree , Metrics]:
-    
     bs = batch.inputs.shape[0]
     min_batchSize = bs // n_minbatch
-    keys = jax.random.split(key , n_minbatch)
+    keys = jax.random.split(key, n_minbatch)
 
-    grad_fn = jax.value_and_grad(loss_fn , has_aux=True)
+    grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
 
-    def _minbatch_step(
-            batch_idx : jax.Array | int
-    ) -> Tuple[Pytree , Metrics]:
-        #take out minibatch using dynamic slicing
+    def _minbatch_step(batch_idx: jax.Array | int) -> Tuple[Pytree, Metrics]:
+        # take out minibatch using dynamic slicing
         minibatch = jax.tree_util.tree_map(
-            lambda x : jax.lax.dynamic_slice_in_dim(
-                x , start_index=batch_idx * min_batchSize,
+            lambda x: jax.lax.dynamic_slice_in_dim(
+                x,
+                start_index=batch_idx * min_batchSize,
                 slice_size=min_batchSize,
-                axis=0
+                axis=0,
             ),
             batch,
         )
         # apply grad
-        (_ , step_metric) , step_grad = grad_fn(
-            state.params , 
-            state.apply_fn,
-            minibatch,
-            keys[batch_idx]
+        (_, step_metric), step_grad = grad_fn(
+            state.params, state.apply_fn, minibatch, keys[batch_idx]
         )
 
-        return step_grad , step_metric
-    
+        return step_grad, step_metric
 
     def _scan_step(
-            carry : Tuple[Pytree , Metrics],
-            batch_idx : jax.Array | int,
+        carry: Tuple[Pytree, Metrics],
+        batch_idx: jax.Array | int,
+    ) -> Tuple[Tuple[Pytree, Metrics], None]:
 
-    ) -> Tuple[Tuple[Pytree , Metrics] , None]:
-        
-        step_grads , step_metrics = _minbatch_step(batch_idx)
-        carry = jax.tree_util.tree_map(
-            jnp.add ,
-            carry,
-            (step_grads , step_metrics)
-        )
+        step_grads, step_metrics = _minbatch_step(batch_idx)
+        carry = jax.tree_util.tree_map(jnp.add, carry, (step_grads, step_metrics))
 
-        return carry , None
-    
+        return carry, None
 
-    grads_shapes , metrics_shape = jax.eval_shape(_minbatch_step , 0)
+    grads_shapes, metrics_shape = jax.eval_shape(_minbatch_step, 0)
 
-    grads = jax.tree_util.tree_map(
-        lambda x : jnp.zeros(x.shape , x.dtype) ,
-        grads_shapes
-    )
+    grads = jax.tree_util.tree_map(lambda x: jnp.zeros(x.shape, x.dtype), grads_shapes)
 
     metrics = jax.tree_util.tree_map(
-        lambda x : jnp.zeros(x.shape , x.dtype) ,
-        metrics_shape
+        lambda x: jnp.zeros(x.shape, x.dtype), metrics_shape
     )
 
-
-    (grads , metrics) , _ = jax.lax.scan(
-        _scan_step ,
-        init=(grads , metrics),
-        xs=jnp.arange(n_minbatch),
-        length=n_minbatch
+    (grads, metrics), _ = jax.lax.scan(
+        _scan_step, init=(grads, metrics), xs=jnp.arange(n_minbatch), length=n_minbatch
     )
 
-    grads = jax.tree_util.tree_map(
-        lambda g : g / n_minbatch,
-        grads
-    )
+    grads = jax.tree_util.tree_map(lambda g: g / n_minbatch, grads)
 
-    return grads , metrics
-
+    return grads, metrics
 
 
 def accum_grads(
@@ -170,7 +145,7 @@ def accum_grads(
     loss_fn: Callable,
     use_scan: bool = False,
 ) -> Tuple[Pytree, Metrics]:
-    
+
     if use_scan:
 
         return accum_grads_scan(
@@ -178,9 +153,9 @@ def accum_grads(
             batch=batch,
             key=key,
             n_minbatch=num_minibatches,
-            loss_fn=loss_fn
+            loss_fn=loss_fn,
         )
-    
+
     else:
 
         return accum_grads_loop(
@@ -188,16 +163,15 @@ def accum_grads(
             batch=batch,
             key=key,
             n_minbatch=num_minibatches,
-            loss_fn=loss_fn
+            loss_fn=loss_fn,
         )
-    
+
 
 def print_metrics(
-        metrics:Metrics,
-        title : str,
-
+    metrics: Metrics,
+    title: str,
 ) -> None:
-    
+
     metrics = jax.device_get(metrics)
     lines = [f"{k}: {v[0] / v[1]:.6f}" for k, v in metrics.items()]
     if title:
@@ -205,6 +179,7 @@ def print_metrics(
         max_len = max(len(title), max(map(len, lines)))
         lines = [title.center(max_len, "=")] + lines
     print("\n".join(lines))
+
 
 def get_num_params(state: TrainState) -> int:
     return sum(np.prod(x.shape) for x in jax.tree_util.tree_leaves(state.params))
