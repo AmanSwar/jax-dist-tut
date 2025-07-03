b803dba5700121288b88c13dace4288d9e026aa4
Aman <aman.swar001@gmail.com>
2025-07-03 23:44:39 +0530

    add pipe-parallel wrapper for model

--- pipeline_parallel.py
+++ pipeline_parallel.py
@@ -242,6 +242,69 @@ def execute_pipeline(
     return outputs
 
 
+class PipelineModule(nn.Module):
 
+    model_axis_name : str
+    num_microbatches : int
+    module_fn : Callable[... , nn.Module]
 
+    @nn.compact
+    def __call__(
+        self,
+        *args,
+        **kwargs
+    ):
+        
+        module = self.module_fn()
+
+        return execute_pipeline(
+            module,
+            *args,
+            **kwargs,
+            num_microbatches=self.num_microbatches,
+            model_axis_name=self.model_axis_name,
+        )
+    
+class ModelParallelWrapper(nn.Module):
+
+    model_axis_name :str
+    module_fn : Callable[... , nn.Module]
+    mask_except_model_idx : int
+    split_rngs : bool
+    module_kwargs : FrozenDict[str , Any] = FrozenDict({})
+
+    @nn.compact
+    def __call__(self, *args , **kwargs):
+
+        if self.is_initializing() and self.split_rngs:
+            self.scope.rngs["params"] = self.scope.rngs["params"].replace(
+                rng=fold_rng_over_axis(self.scope.rngs["params"].rng , self.model_axis_name)
+            )
+
+            module = nn.map_variables(
+                target=functools.partial(
+                    self.module_fn,
+                    name="shareded",
+                    **self.module_kwargs,
+                ),
+
+                trans_in_fn=functools.partial(unstack_params , axis_name=self.model_axis_name),
+                trans_out_fn= functools.partial(
+                    stack_params,
+                    axis_name=self.model_axis_name,
+                    mask_except=self.mask_except_model_idx,
+                ),
+                mapped_collections="params",
+                mutable=True,
+            )()
+
+            return module(
+                *args,
+                **kwargs
+            )
+        
+
+        
+
+            
 
