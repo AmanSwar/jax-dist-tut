d246960b21bc9016353d8db43d3d7cc0b37c984d
Aman <aman.swar001@gmail.com>
2025-07-03 16:24:10 +0530

    model defination

--- pipeline_parallel.py
+++ pipeline_parallel.py
@@ -37,3 +37,61 @@ from single_gpu import (
 from data_paral import fold_rng_over_axis
 from param_sharding import sync_gradients
 
+
+config = ConfigDict(
+    dict(
+        dtype = jnp.bfloat16
+    )
+)
+
+
+class MLPBlock(nn.Module):
+
+    config : ConfigDict
+    train : bool
+
+    @nn.compact
+    def __call__(
+        self,
+        x : jax.Array
+    ) -> jax.Array:
+
+        input_feat = x.shape[-1]
+        residual = x
+        x = nn.LayerNorm(
+            dtype=self.config.dtype,
+            name="pre_norm"
+        )(x)
+
+        x = nn.Dense(
+            features= self.config.hidden_size * self.config.mlp_expansion,
+            dtype=self.config.dtype,
+            name="input_dense"
+        )(x)
+
+        x = nn.silu(x)
+        x = nn.Dropout(rate=self.config.dropout_rate , deterministic=not self.train)(x)
+        x = nn.Dense(features=input_feat , dtype=self.config.dtype , name="output")(x)
+
+        return x + residual
+
+
+class MLPLayers(nn.Module):
+    config: ConfigDict
+    train: bool
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        # Scan version
+        block_class = MLPBlock
+        if "MLP" in self.config.remat:
+            block_class = nn.remat(block_class, prevent_cse=False)
+        block = block_class(config=self.config, train=self.train, name="block")
+        x, _ = nn.scan(
+            lambda module, carry, _: (module(carry), ()),
+            variable_axes={"params": 0},
+            split_rngs={"params": True, "dropout": True},
+            length=self.config.num_layers,
+        )(block, x, ())
+        
+        return x
