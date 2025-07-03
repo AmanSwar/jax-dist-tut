7b058ec54f51259b98d1ae17fcfda3fc93d6e1a5
Aman <aman.swar001@gmail.com>
2025-07-03 14:24:42 +0530

    init

---
+++ pipeline_parallel.py
@@ -0,0 +1,27 @@
+import jax.experimental
+import jax.experimental.shard_map
+from util import sim_multiCPU_dev
+
+# simulate multiple devices on CPU
+sim_multiCPU_dev()
+
+import functools
+from pprint import pprint
+from typing import Any , Callable , Dict , Tuple
+
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+from flax.core.frozen_dict import FrozenDict
+from jax.experimental.shard_map import shard_map
+from jax.sharding import Mesh
+from jax.sharding import PartitionSpec as P
+from ml_collections import ConfigDict
+
+
+PyTree = Any
+Parameter = jax.Array | nn.Partitioned
+Metrics = Dict[str , Tuple[jax.Array , ...]]
+
