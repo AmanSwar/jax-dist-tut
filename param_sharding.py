0670e244b8ee8311a48ecbfab4718b5aa2eeb0af
Aman <aman.swar001@gmail.com>
2025-07-03 12:52:00 +0530

    add initialization

--- param_sharding.py
+++ param_sharding.py
@@ -17,7 +17,7 @@ from jax.sharding import PartitionSpec as P
 from ml_collections import ConfigDict
 from absl import logging
 
-from util import Pytree
+from util import Pytree , TrainState , Batch
 
 MODEL_CONFIG = ConfigDict(
     dict(
@@ -26,13 +26,23 @@ MODEL_CONFIG = ConfigDict(
         dtype=jnp.bfloat16,
         num_classes=DATA_CONFIG.num_classes,
         data_axis_name="data",
+        lr=1e-4
     )
 )
 
+DATA_CONFIG = ConfigDict(
+    dict(
+        batch_size=128,
+        num_classes=10,
+        input_size=784,
+    )
+)
 
 config = ConfigDict(
     dict(
-        model = MODEL_CONFIG
+        model = MODEL_CONFIG,
+        data = DATA_CONFIG,
+        seed=6969,
     )
 )
 
@@ -173,7 +183,7 @@ class Classifier(nn.Module):
 
         sharded_dense = shard_module_params(
             nn.Dense,
-            axis_name=self.config.data_axis_name,
+            axis_name=self.config.model.data_axis_name,
             min_weight_size=self.config.min_weight_size,
         )
 
@@ -197,5 +207,67 @@ class Classifier(nn.Module):
         return x
 
 
-config.model.min_weight_size = 2**4
+def init_model(
+        rng,
+        x : jax.Array,
+        model : nn.Module
+) -> TrainState:
+    init_rng , rng = jax.random.split(rng)
+    var = model.init({'params' : init_rng} , x , train=False)
+    params = var.pop("params")
 
+    state = TrainState.create(
+        apply_fn=model.apply,
+        params=params,
+        tx=optax.adamw(
+            learning_rate=config.model.lr,
+        ),
+        rng=rng,
+    )
+
+    return state
+
+
+config.model.min_weight_size = 2**4  #set to smaller one cuz this is a experimental script
+
+model_fsdp = Classifier(config=config.model)
+devices = np.array(jax.devices())
+# init mesh
+mesh = Mesh(devices, (config.data_axis_name,))
+
+init_fsdp_fn = shard_map(
+    functools.partial(init_model , model=model_fsdp),
+    mesh,
+    in_specs=(P() , P(config.model.data_axis_name)),
+    out_specs=P(),
+    check_rep=False,
+)
+
+rng = jax.random.PRNGKey(config.seed)
+
+model_init_rng, data_inp_rng, datal_label_rng = jax.random.split(rng, 3)
+
+state_fsdp_shape = jax.eval_shape(init_fsdp_fn)
+state_fsdp_specs = nn.get_partition_spec(state_fsdp_shape)
+
+init_fsdp_fn = shard_map(
+    functools.partial(init_model, model=model_fsdp),
+    mesh,
+    in_specs=(P(), P(config.model.data_axis_name)),
+    out_specs=state_fsdp_specs,
+    check_rep=False,
+)
+
+batch = Batch(
+    inputs=jax.random.normal(
+        data_inp_rng,
+        (config.data.batch_size, config.data.input_size),  # type: ignore
+    ),
+    labels=jax.random.normal(
+        datal_label_rng,
+        (config.data.batch_size,),
+        0,
+        config.data.num_classes,
+    ),
+)
+state_fsdp = init_fsdp_fn(model_init_rng, batch.inputs)
