9ee398ecd4f408f6832cfaf94a493ae0178d624b
Aman <aman.swar001@gmail.com>
2025-08-24 23:04:36 +0530

    add function prepare module

--- tensor_parallel_transformer.py
+++ tensor_parallel_transformer.py
@@ -158,3 +158,19 @@ class TPMultiHeadAttn(nn.Module):
             name="out",
         )(x)
         return x
+
+
+def prepare_module(
+    layer: Callable[..., nn.Module], layer_name: str, config: ConfigDict
+) -> Callable[..., nn.Module]:
+    if config.get("fsdp", None) is not None and layer_name in config.fsdp.modules:
+        layer = shard_module_params(
+            layer,
+            axis_name=config.data_axis_name,
+            min_weight_size=config.fsdp.min_weight_size,
+        )
+
+    if config.get("remat", None) is not None and layer_name in config.remat:
+        layer = nn.remat(layer, prevent_cse=False)
+    
+    return layer
