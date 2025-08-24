5edbc211cc605122489f519e0f83dda031514f09
Aman <aman.swar001@gmail.com>
2025-08-24 20:24:36 +0530

    add function async gather bidirectional

--- tensor_parallel_async.py
+++ tensor_parallel_async.py
@@ -31,4 +31,30 @@ def async_gather(
     for _ in range(1, tp_size):
             p = jax.lax.ppermute(p, axis_name, perm=shift_perm)
             ps.append(p)
-        return ps
+    return ps
+
+def async_gather_bidirectional(
+    x: jax.Array, axis_name: str, shift_up: bool = True
+) -> List[jax.Array]:
+
+    tp_size = jax.lax.psum(1, axis_name)
+    shift_up_perm = [(j, (j + 1) % tp_size) for j in range(tp_size)]
+    shift_down_perm = [(j, (j - 1) % tp_size) for j in range(tp_size)]
+    ps_up = []
+    ps_down = []
+    p_up = x
+    p_down = x
+
+    for i in range(1, tp_size):
+        if i % 2 == 0:
+            p_down = jax.lax.ppermute(p_down, axis_name=axis_name, perm=shift_down_perm)
+            ps_down.append(p_down)
+        else:
+            p_up = jax.lax.ppermute(p_up, axis_name=axis_name, perm=shift_up_perm)
+            ps_up.append(p_up)
+
+    if shift_up:
+        ps = [x] + ps_up + ps_down[::-1]
+    else:
+        ps = [x] + ps_down + ps_up[::-1]
+    return ps
