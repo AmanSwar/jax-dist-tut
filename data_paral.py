b819038808a98708d81df334e3eff80239777bed
Aman <aman.swar001@gmail.com>
2025-08-24 20:46:26 +0530

    add function shard_params

--- data_paral.py
+++ data_paral.py
@@ -1,5 +1,5 @@
 from util import sim_multiCPU_dev
-#simulate multiple devices on CPU
+# simulate multiple devices on CPU
 sim_multiCPU_dev()
 
 
@@ -36,8 +36,7 @@ def fold_rng_over_axis(
     return jax.random.fold_in(rng , axis_index)
 
 
-
-#configs
+# configs
 DATA_CONFIG = ConfigDict(
     dict(
         batch_size=128,
@@ -102,7 +101,7 @@ class Classifier(nn.Module):
         x = x.astype(jnp.float32)
 
         return x
-    
+
 # init
 model_dp = Classifier(config=CONFIG.model)
 optimizer = optax.adamw(
@@ -124,7 +123,6 @@ batch = Batch(
         CONFIG.DATA_CONFIG.num_classes
     ),
 )
-
 
 
 def init_dp(
@@ -157,10 +155,10 @@ def init_dp(
 
 
 devices = np.array(jax.devices())
-#init mesh
+# init mesh
 mesh = Mesh(devices , (CONFIG.data_axis_name,))
 
-#initialize sharding
+# initialize sharding
 init_dp_fn = jax.jit(
     shard_map(
         functools.partial(init_dp , model=model_dp),
@@ -196,7 +194,6 @@ def loss_fn(
     loss = loss.mean()
 
     return loss , step_metrics
-
 
 
 def train_step_dp(
@@ -260,6 +257,58 @@ train_step_dp_fn = jax.jit(
 )
 
 
+@jax.named_scope("shard_params")
+def shard_params(
+    params: PyTree, axis_name: str, min_weight_size: int = 2**18
+) -> PyTree:
+    
+    axis_idx = jax.lax.axis_index(axis_name)
+    axis_size = jax.lax.psum(1, axis_name)
+
+    def _split(x: Parameter) -> Parameter:
+        if isinstance(x, nn.Partitioned):
+            value, names = x.value, x.names
+        else:
+            value = x
+            names = (None,) * value.ndim
+        if axis_name in names:
+            logging.warning(
+                f"Parameter {value.shape} with names {names} already sharded on axis {axis_name}."
+            )
+            return x
+        elif value.size <= min_weight_size:
+            logging.info(
+                f"Parameter {value.shape} with names {names} too small to shard, size {value.size} < {min_weight_size}."
+            )
+            return x
+        else:
+            shape = value.shape
+            idx = np.argsort(shape)[::-1]  # Shard along largest possible axis.
+            for i in idx:
+                if shape[i] % axis_size == 0 and names[i] is None:
+                    split_size = shape[i] // axis_size
+                    p_sharded = nn.Partitioned(
+                        value=lax.dynamic_slice_in_dim(  # Shard to keep on present device.
+                            value, axis_idx * split_size, split_size, axis=i
+                        ),
+                        names=names[:i] + (axis_name,) + names[i + 1 :],
+                    )
+                    return p_sharded
+            logging.warning(
+                f"Could not shard {value.shape} with names {names} on axis {axis_name}, no suitable axis found."
+            )
+            return x
+
+    return jax.tree_util.tree_map(
+        _split,
+        params,
+        is_leaf=lambda x: isinstance(
+            x, nn.Partitioned
+        ),  # Consider a nn.Partitioned object as a leaf.
+    )
+
+
+
 def sync_grads(
     grads : Pytree,
     axis_names = Sequence[str]
@@ -289,8 +338,6 @@ def sync_grads(
     )
 
 
-
-
 _ , metric_shape = jax.eval_shape(
     train_step_dp_fn,
     state_dp,
@@ -306,7 +353,7 @@ metrics_dp = jax.tree_util.tree_map(
     ),
     metric_shape,)    
 
-#main loop 
+# main loop
 for _ in range(10):
 
     state_dp , metrics_dp = train_step_dp_fn(state_dp , metrics_dp , batch)
@@ -314,6 +361,3 @@ for _ in range(10):
 final_metrics_dp = jax.tree_util.tree_map(lambda x: jnp.zeros(x.shape, dtype=x.dtype), metric_shape)
 state_dp, final_metrics_dp = train_step_dp_fn(state_dp, final_metrics_dp, batch)
 print_metrics(final_metrics_dp , title="dp")
-
-
-        
