23953106c43850b0a7df24ba9556133b91002ede
Aman <aman.swar001@gmail.com>
2025-08-24 23:00:30 +0530

    add dot product attention

--- tensor_parallel_transformer.py
+++ tensor_parallel_transformer.py
@@ -27,6 +27,7 @@ class QKVDense(nn.Module):
 
     @nn.compact
     def __call__(self, x: jax.Array) -> Tuple[jax.Array, jax.Array, jax.Array]:
+
         q = nn.DenseGeneral(
             (self.num_heads, self.head_dim),
             kernel_init=self.kernel_init,
@@ -34,6 +35,7 @@ class QKVDense(nn.Module):
             dtype=self.config.dtype,
             name="query",
         )(x)
+
         k = nn.DenseGeneral(
             (self.num_heads, self.head_dim),
             kernel_init=self.kernel_init,
@@ -41,6 +43,7 @@ class QKVDense(nn.Module):
             dtype=self.config.dtype,
             name="key",
         )(x)
+
         v = nn.DenseGeneral(
             (self.num_heads, self.head_dim),
             kernel_init=self.kernel_init,
@@ -54,8 +57,40 @@ class QKVDense(nn.Module):
                 dtype=self.config.dtype,
                 name="query_norm",
             )(q)
+
             k = nn.RMSNorm(
                 dtype=self.config.dtype,
                 name="key_norm",
             )(k)
+
         return q, k, v
+
+def dot_product_attention(
+    query: jax.Array,
+    key: jax.Array,
+    value: jax.Array,
+    mask: jax.Array | None,
+    softmax_dtype: jnp.dtype = jnp.float32,
+):
+
+    num_features = query.shape[-1]
+
+    dtype = query.dtype
+    scale = num_features**-0.5
+    
+    query = query * scale
+    query = query.astype(softmax_dtype)
+    
+    key = key.astype(softmax_dtype)
+    weights = jnp.einsum("...qhd,...khd->...hqk", query, key)
+    
+    if mask is not None:
+        weights = jnp.where(mask, weights, jnp.finfo(softmax_dtype).min)
+    
+    weights = nn.softmax(weights, axis=-1)
+    weights = weights.astype(dtype)
+    
+    new_vals = jnp.einsum("...hqk,...khd->...qhd", weights, value)
+    new_vals = new_vals.astype(dtype)
+    
+    return new_vals
