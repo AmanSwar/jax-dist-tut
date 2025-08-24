99cb79a115dead4f987970043ce30f2ee8b3f337
Aman <aman.swar001@gmail.com>
2025-08-24 20:51:39 +0530

    add QKVDEnse module

--- tensor_parallel_transformer.py
+++ tensor_parallel_transformer.py
@@ -16,3 +16,46 @@ from tensor_parallel_async import TPAsyncDense ,TPAsyncMLPBlock , TPNorm
 PyTree = Any
 Parameter = jax.Array | nn.Partitioned
 Metrics = Dict[str, Tuple[jax.Array, ...]]
+
+
+class QKVDense(nn.Module):
+    config: ConfigDict
+    num_heads: int
+    head_dim: int
+    kernel_init: Callable
+    use_bias: bool = False
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> Tuple[jax.Array, jax.Array, jax.Array]:
+        q = nn.DenseGeneral(
+            (self.num_heads, self.head_dim),
+            kernel_init=self.kernel_init,
+            use_bias=False,
+            dtype=self.config.dtype,
+            name="query",
+        )(x)
+        k = nn.DenseGeneral(
+            (self.num_heads, self.head_dim),
+            kernel_init=self.kernel_init,
+            use_bias=False,
+            dtype=self.config.dtype,
+            name="key",
+        )(x)
+        v = nn.DenseGeneral(
+            (self.num_heads, self.head_dim),
+            kernel_init=self.kernel_init,
+            use_bias=False,
+            dtype=self.config.dtype,
+            name="value",
+        )(x)
+
+        if self.config.normalize_qk:
+            q = nn.RMSNorm(
+                dtype=self.config.dtype,
+                name="query_norm",
+            )(q)
+            k = nn.RMSNorm(
+                dtype=self.config.dtype,
+                name="key_norm",
+            )(k)
+        return q, k, v
