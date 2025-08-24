da07e743cfd68177984b2dcd2bc55787f062ea26
Aman <aman.swar001@gmail.com>
2025-08-24 20:23:37 +0530

    add function async_gather

--- tensor_parallel_async.py
+++ tensor_parallel_async.py
@@ -15,3 +15,20 @@ Parameter = jax.Array | nn.Partitioned
 Metrics = Dict[str, Tuple[jax.Array, ...]]
 
 
+def async_gather(
+    x  : PyTree,
+    axis_name : str,
+    shift_up : bool = True
+) -> List[PyTree]:
+
+    tp_size = jax.lax.psum(1, axis_name)
+    if shift_up:
+        shift_perm = [(j, (j + 1) % tp_size) for j in range(tp_size)]
+    else:
+        shift_perm = [(j, (j - 1) % tp_size) for j in range(tp_size)]
+    ps = [x]
+    p = x
+    for _ in range(1, tp_size):
+            p = jax.lax.ppermute(p, axis_name, perm=shift_perm)
+            ps.append(p)
+        return ps
