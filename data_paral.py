53697b2ca66cd6706ef5a7f6fe61cad69e267874
Aman <aman.swar001@gmail.com>
2025-08-24 20:05:37 +0530

    add function sync_grads

--- data_paral.py
+++ data_paral.py
@@ -264,7 +264,31 @@ def sync_grads(
     grads : Pytree,
     axis_names = Sequence[str]
 ):
-    def _sync_grads(g : )
+    def _sync_grads(g : Parameter):
+
+        if isinstance(g , nn.Partitioned):
+            replication_axis_name = [
+                name for name in axis_names if name not in jax.tree_util.tree_leaves(g.names)
+            ]
+
+
+            if len(replication_axis_name) == 0:
+                return g
+            
+
+            else:
+                return g.replace(
+                    value=jax.lax.pmean(g.value , axis_name=replication_axis_name)
+                )
+        else:
+            return jax.lax.pmean(g , axis_name=axis_names)
+        
+
+    return jax.tree_util.tree_map(
+        _sync_grads , grads , is_leaf= lambda x : isinstance(x , nn.Partitioned)
+    )
+
+
 
 
 _ , metric_shape = jax.eval_shape(
